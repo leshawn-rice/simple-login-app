9bcb04e7bee6eca582a7d1803bb2bfbbb6988fb8
Leshawn <leshawnrice3@gmail.com>
2020-10-22 12:46:44 -0700

    Finished class unit tests

--- test_classes.py
+++ test_classes.py
@@ -2,16 +2,30 @@ from user_classes import UserList, User
 from unittest import TestCase
 
 
+def ftest_get_users_from_file(file):
+    users = []
+    for line in file:
+        user_info = line.split(':')
+        users.append(user_info)
+    return users
+
+
 class UserListTestCase(TestCase):
 
     def setUp(self):
         self.test_list = UserList()
         self.test_filename = 'static/test_users.txt'
-        self.test_users = ['username', 'leshawnrice', 'kellieconnors']
+        self.test_empty_filename = 'static/test_empty_users.txt'
+        self.test_usernames = ['username', 'leshawnrice', 'kellieconnors']
         self.test_passwords = ['Password1', 'Louis123', 'Kellie321']
         self.test_created_ats = ['01/01/0001 at 00.00.00',
                                  '10/22/2020 at 15.17.28',
                                  '10/20/2020 at 08.39.11']
+
+    def tearDown(self):
+        file = open(self.test_empty_filename, 'w')
+        file.write('')
+        file.close()
 
     def test_instantiation(self):
         self.assertIsInstance(self.test_list, UserList)
@@ -24,6 +38,66 @@ class UserListTestCase(TestCase):
         self.assertEqual(len(self.test_list.users), 3)
 
         for user in self.test_list.users:
-            self.assertIn(user.username, self.test_users)
+            self.assertIn(user.username, self.test_usernames)
             self.assertIn(user.password, self.test_passwords)
             self.assertIn(user.created_at, self.test_created_ats)
+
+    def test_write_users_to_file(self):
+        self.test_list.get_users_from_file(self.test_filename)
+        self.test_list.write_users_to_file(self.test_empty_filename)
+
+        file = open(self.test_empty_filename, 'r')
+        users = ftest_get_users_from_file(file)
+        file.close()
+
+        for user in users:
+            self.assertIn(user[0].strip(), self.test_usernames)
+            self.assertIn(user[1].strip(), self.test_passwords)
+            self.assertIn(user[2].strip(), self.test_created_ats)
+
+    def test_check_user_in_list(self):
+        self.test_list.get_users_from_file(self.test_filename)
+        for username in self.test_usernames:
+            self.assertIsInstance(
+                self.test_list.check_user_in_list(username), User)
+            self.assertEqual(
+                username, self.test_list.check_user_in_list(username).username)
+        self.assertIsNone(self.test_list.check_user_in_list('billy'))
+
+    def test_add_user_to_list(self):
+        self.test_list.add_user_to_list('testuser', 'testpassword')
+        for user in self.test_list.users:
+            self.assertIsInstance(user, User)
+            self.assertEqual('testuser', user.username)
+            self.assertEqual('testpassword', user.password)
+
+
+class UserTestCase(TestCase):
+    def setUp(self):
+        self.test_user = User('testname', 'testpassword')
+        self.test_filename = 'static/test_empty_users.txt'
+
+    def tearDown(self):
+        file = open(self.test_filename, 'w')
+        file.write('')
+        file.close()
+
+    def test_instantiation(self):
+        self.assertIsInstance(self.test_user, User)
+        self.assertEqual(self.test_user.username, 'testname')
+        self.assertEqual(self.test_user.password, 'testpassword')
+        self.assertIsInstance(self.test_user.created_at, str)
+
+    def test_get_created_time(self):
+        self.assertIsInstance(self.test_user.get_created_time(), str)
+        self.assertEqual(len(self.test_user.get_created_time()), 22)
+
+    def test_write_to_file(self):
+        self.test_user.write_to_file(self.test_filename)
+        file = open(self.test_filename)
+        users = ftest_get_users_from_file(file)
+        file.close()
+        for user in users:
+            self.assertEqual(user[0].strip(), 'testname')
+            self.assertEqual(user[1].strip(), 'testpassword')
+            self.assertEqual(len(user[2].strip()), 22)
